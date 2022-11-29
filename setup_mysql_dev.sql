6fb8e96b5c8064f279b8c320b28de292f8dcf4d5
otalorajuand <otalorajuand@gmail.com>
2022-11-29 20:17:31 +0000

    Addition of task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
--- creates the MySQL server user user_0d_1.
+-- prepares a MySQL server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
