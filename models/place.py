c9cf44699607ac1f6d6ab154860f555ed959287e
otalorajuand <otalorajuand@gmail.com>
2022-11-30 21:30:25 +0000

    Addition of task 10

--- models/place.py
+++ models/place.py
@@ -9,7 +9,7 @@ from models.review import Review
 
 metadata = Base.metadata
 
-place_amenity = Table('PlaceAmenity', metadata,
+place_amenity = Table('place_amenity', metadata,
     Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
     Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
 )
