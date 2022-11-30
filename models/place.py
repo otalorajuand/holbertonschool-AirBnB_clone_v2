1537d642a439e8ec4a0a5d4039bf1293cb06c5e2
otalorajuand <otalorajuand@gmail.com>
2022-11-30 21:20:28 +0000

    Addition of task 10

--- models/place.py
+++ models/place.py
@@ -6,6 +6,13 @@ from sqlalchemy.orm import relationship
 import os 
 from models.review import Review
 
+metadata = Base.metadata
+
+place_amenity = Table('place_amenity', metadata,
+    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
+)
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -22,9 +29,20 @@ class Place(BaseModel, Base):
     longitude = Column(Float)
     amenity_ids = []
     reviews = relationship("Review", backref="place", cascade="delete")
+    amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
 
     if os.getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def reviews(self):
             from models import storage
             return [review for review in list(storage.all(Review).values()) if review.place_id == self.id]
+
+        @property
+        def amenities(self):
+            from models import storage
+            return [amenity for amenity in list(storage.all(Amenity).values()) if amenity.id in self.amenity_ids]
+
+        @setter.amenities
+        def amenities(self, obj):
+            if obj is not None:
+                self.amenity_ids.append(obj.id)
