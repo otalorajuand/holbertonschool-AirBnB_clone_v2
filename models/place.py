b8f3140167c30712b75d4f198a6ce4afb725fd44
otalorajuand <otalorajuand@gmail.com>
2022-11-30 21:52:51 +0000

    Addition of task 10

--- models/place.py
+++ models/place.py
@@ -6,12 +6,11 @@ from sqlalchemy.orm import relationship
 import os 
 from models.review import Review
 
-
-PlaceAmenity = Table('place_amenity', Base.metadata,
+metadata = Base.metadata
+place_amenity = Table('place_amenity', metadata,
     Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
     Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
 )
-
 
 
 class Place(BaseModel, Base):
