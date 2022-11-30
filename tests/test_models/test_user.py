3a7fd0a9a9ce1c15ee689016dbd591900d92d851
otalorajuand <otalorajuand@gmail.com>
2022-11-30 22:18:59 +0000

    Addition of tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -16,19 +16,23 @@ class test_User(test_basemodel):
     def test_first_name(self):
         """ """
         new = self.value()
+        new.first_name = "Juan"
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
         """ """
         new = self.value()
+        new.last_name = "Otalora"
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
         """ """
         new = self.value()
+        new.email = "Juan@.com"
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
         """ """
         new = self.value()
+        new.password = "fsdfdsf"
         self.assertEqual(type(new.password), str)
