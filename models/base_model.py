1605f0862d5afbda09f19332313c9537211ae8cb
abimont <abimontes00@gmail.com>
2022-12-02 12:15:31 -0500

    pep8 style Basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -24,11 +24,11 @@ class BaseModel:
 
         if kwargs:
             if kwargs.get('updated_at'):
-                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['updated_at'] = datetime.strptime(
+                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
             if kwargs.get('created_at'):
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime.strptime(
+                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
             if kwargs.get('__class__'):
                 del kwargs['__class__']
             self.__dict__.update(kwargs)
