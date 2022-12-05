615167588a9394ac2d47e459ce45369c0899a6ef
otalorajuand <otalorajuand@gmail.com>
2022-12-05 20:46:49 +0000

    Addition of task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -68,7 +68,7 @@ class DBStorage:
         session_factory = sessionmaker(
             bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
-        self.__session = Session()
+        self.__session = Session
 
     def close(self):
         """method on the private session attribute"""
