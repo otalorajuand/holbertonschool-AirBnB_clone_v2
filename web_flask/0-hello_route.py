dc9849b02d5f368177ddcee0490180cb88d920c7
otalorajuand <otalorajuand@gmail.com>
2022-12-05 16:03:54 +0000

    Addition of task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
+    """defines the return for the index"""
     return "Hello HBNB!"
 
 app.run(host='0.0.0.0', port=5000)
