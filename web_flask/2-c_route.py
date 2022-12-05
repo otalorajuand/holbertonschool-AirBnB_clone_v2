7998b9eee98c1fd4e06496fa6fb9344ec8da5278
otalorajuand <otalorajuand@gmail.com>
2022-12-05 16:49:00 +0000

    Addition of task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,7 +21,7 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    """defines the return the /c/<text> url"""
+    """display “C ” followed by the value of the text variable"""
     text = text.replace("_", " ")
     return f'C {text}'
 
