c35fb911355469e3e7b96a2055c102574d44ac1e
otalorajuand <otalorajuand@gmail.com>
2022-12-07 21:43:16 +0000

    Addition of task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,7 +9,7 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def states_list():
     """display a HTML with the states of the db"""
     states = storage.all(State)
